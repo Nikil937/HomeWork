487b78005a904b640b2f545bc7630c6c1a3f1ea9
nikilka <linsr9370@gmail.com>
2026-03-03 22:05:28 +0300

    4-navigator

---
+++ 4-navigator.js
@@ -0,0 +1,11 @@
+const startlat = 3;
+const startlong = 3;
+const endlat = 7;
+const endlong = 7;
+const answer = 0;
+
+answer = ((endlat - startlat)**2 + (endlong - startlong)**2)**1/2;
+console.log(answer);
+
+
+
