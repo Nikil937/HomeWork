a7b3b6d33f70e7212ebd04c70235d198cb34edce
nikilka <linsr9370@gmail.com>
2026-03-06 21:53:04 +0300

    7-converter

---
+++ 7-converter.js
@@ -0,0 +1,14 @@
+function MoneyChange(summ, currentcurrency, tocurrency) {
+    switch(true) {
+        case currentcurrency === 'RUB' && tocurrency === 'USD':
+            return summ * 77;
+            break;
+        case currentcurrency === 'USD' && tocurrency === 'RUB':
+            return summ / 77;
+            break;
+        default:
+            return null;
+    }
+}
+
+MoneyChange(1000,'RUB','USD')
